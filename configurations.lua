a114d075c9f303da9bd37f819e151eadc40ab362
LH_Lawliet <35814039+LH-Lawliet@users.noreply.github.com>
2018-08-07 23:16:24 +0200

    Update configurations.lua

--- configurations.lua
+++ configurations.lua
@@ -2,8 +2,8 @@
 -- @License: GNU General Public License v3.0
 
 Config = {
-  kmhMode = true,
-  Rect_alpha = 100,
-  Speed_alpha = 150,
-  Advanced_for_plane = true,
+  KmhMode = true,
+  RectAlpha = 100,
+  SpeedAlpha = 150,
+  AdvancedForPlane = true,
 }
