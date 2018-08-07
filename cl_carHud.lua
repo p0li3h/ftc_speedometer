a06fbb6ce69af6802600796d622ba8ded950c7d2
LH_Lawliet <35814039+LH-Lawliet@users.noreply.github.com>
2018-08-07 23:16:02 +0200

    Update cl_carHud.lua
    rename var to be logic with ft_libs

--- cl_carHud.lua
+++ cl_carHud.lua
@@ -23,29 +23,29 @@ Citizen.CreateThread(function()
 
 				if player == pasInSeat then
 
-					if Config.Advanced_for_plane == true and (GetVehicleClass(vehicle) == 15 or GetVehicleClass(vehicle) == 16) then
+					if Config.AdvancedForPlane == true and (GetVehicleClass(vehicle) == 15 or GetVehicleClass(vehicle) == 16) then
 
-						DrawRect(0.085, 0.763, 0.141, 0.088, 0, 0, 0, Config.Rect_alpha)
+						DrawRect(0.085, 0.763, 0.141, 0.088, 0, 0, 0, Config.RectAlpha)
 
 						local carSpeed = GetEntitySpeedVector(vehicle).z  -- convert in km/h or mph
-						exports.ft_libs:Text({ text = "Vertical Speed", font = 4, x = 0.016, y = 0.72, scale = 0.35, alpha = Config.Speed_alpha})
-						exports.ft_libs:Text({ text = ""..math.floor(carSpeed), font = 4, x = 0.016, y = 0.74, scale = 0.55, alpha = Config.Speed_alpha})
-						exports.ft_libs:Text({ text = "M/S", font = 4, x = 0.04, y = 0.745, scale = 0.4, alpha = Config.Speed_alpha})
+						exports.ft_libs:Text({ text = "Vertical Speed", font = 4, x = 0.016, y = 0.72, scale = 0.35, alpha = Config.SpeedAlpha})
+						exports.ft_libs:Text({ text = ""..math.floor(carSpeed), font = 4, x = 0.016, y = 0.74, scale = 0.55, alpha = Config.SpeedAlpha})
+						exports.ft_libs:Text({ text = "M/S", font = 4, x = 0.04, y = 0.745, scale = 0.4, alpha = Config.SpeedAlpha})
 
 						local carSpeed = (GetEntitySpeed(vehicle) * speedMultiplicator) - (math.sqrt((carSpeed*speedMultiplicator)*(carSpeed*speedMultiplicator)))
-						exports.ft_libs:Text({ text = "Horizontal Speed", font = 4, x = 0.086, y = 0.72, scale = 0.35, alpha = Config.Speed_alpha})
-						exports.ft_libs:Text({ text = ""..math.floor(carSpeed), font = 4, x = 0.086, y = 0.74, scale = 0.55, alpha = Config.Speed_alpha})
-						exports.ft_libs:Text({ text = text, font = 4, x = 0.11, y = 0.745, scale = 0.4, alpha = Config.Speed_alpha})
+						exports.ft_libs:Text({ text = "Horizontal Speed", font = 4, x = 0.086, y = 0.72, scale = 0.35, alpha = Config.SpeedAlpha})
+						exports.ft_libs:Text({ text = ""..math.floor(carSpeed), font = 4, x = 0.086, y = 0.74, scale = 0.55, alpha = Config.SpeedAlpha})
+						exports.ft_libs:Text({ text = text, font = 4, x = 0.11, y = 0.745, scale = 0.4, alpha = Config.SpeedAlpha})
 
-						exports.ft_libs:Text({ text = "Height : ", font = 4, x = 0.016, y = 0.78, scale = 0.35, alpha = Config.Speed_alpha})
-						exports.ft_libs:Text({ text = '' .. math.floor(GetEntityHeightAboveGround(vehicle)), font = 4, x = 0.043, y = 0.77, scale = 0.64, alpha = Config.Speed_alpha})
-						exports.ft_libs:Text({ text = "M", font = 4, x = 0.073, y = 0.78, scale = 0.4, alpha = Config.Speed_alpha})
+						exports.ft_libs:Text({ text = "Height : ", font = 4, x = 0.016, y = 0.78, scale = 0.35, alpha = Config.SpeedAlpha})
+						exports.ft_libs:Text({ text = '' .. math.floor(GetEntityHeightAboveGround(vehicle)), font = 4, x = 0.043, y = 0.77, scale = 0.64, alpha = Config.SpeedAlpha})
+						exports.ft_libs:Text({ text = "M", font = 4, x = 0.073, y = 0.78, scale = 0.4, alpha = Config.SpeedAlpha})
 
 					else
 						local carSpeed = GetEntitySpeed(vehicle) * speedMultiplicator -- convert in km/h or mph
-						DrawRect(0.133, 0.947, 0.046, 0.03, 0, 0, 0, Config.Rect_alpha)
-						exports.ft_libs:Text({ text = "" .. math.floor(carSpeed), font = 4, x = 0.11, y = 0.925, scale = 0.64, alpha = Config.Speed_alpha})
-						exports.ft_libs:Text({ text = text, font = 4, x = 0.133, y = 0.937, scale = 0.4, alpha = Config.Speed_alpha})
+						DrawRect(0.133, 0.947, 0.046, 0.03, 0, 0, 0, Config.RectAlpha)
+						exports.ft_libs:Text({ text = "" .. math.floor(carSpeed), font = 4, x = 0.11, y = 0.925, scale = 0.64, alpha = Config.SpeedAlpha})
+						exports.ft_libs:Text({ text = text, font = 4, x = 0.133, y = 0.937, scale = 0.4, alpha = Config.SpeedAlpha})
 					end
 
 				end
